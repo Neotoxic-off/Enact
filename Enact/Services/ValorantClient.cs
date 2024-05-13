9f964279fef84630aaa2c6f970e6cb37eb59127a
neo <neotoxic.off@gmail.com>
2024-05-13 18:03:38 +0200

    Init client

---
+++ Enact/Services/ValorantClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enact.Services
+{
+    public class ValorantClient
+    {
+        private HttpClient Client { get; set; }
+
+        public ValorantClient()
+        {
+            Client = new HttpClient();
+        }
+
+        private async void Status()
+        {
+            var responseString = await Client.GetStreamAsync("http://www.example.com/recepticle.aspx");
+        }
+    }
+}
---
+++ Enact/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enact.ViewModels
+{
+    public class MainViewModel : Interfaces.INotified
+    {
+    }
+}
